9fcd053429f2e454bfc0e4c3df04b3eb90638976
PaninaAY <PaninaAY@bk.ru>
2023-12-28 00:40:32 +0200

    everything done, but I did not get pictures

--- my-react-app/src/MainPage/CardList.jsx
+++ my-react-app/src/MainPage/CardList.jsx
@@ -1,18 +1,26 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import s from './productsList.module.css';
-import camera_photo from '../assets/camera_photo.jpg';
 import { Products } from "../products";
 
-
+// function endPrice() {
+//     let endDiscount = { ProductItem.price } * { ProductItem.discount }
+//     return endDiscount;
+// }
 
 const CardList = (props) => {
+
+    //endPrice = { ProductItem.price } * { ProductItem.discount }
 
     const ProductListItems = Products.map(ProductItem =>
         <li key={ProductItem.id} >
             <div className={s.item_card}>
                 <img className={s.item_img} src={ProductItem.imageURL} alt={ProductItem.title} />
-                <span className={s.price}>{ProductItem.price}</span>
+                <div className={s.price_counting} >
+                    <span className={s.price}> {ProductItem.discount === 1 ? ProductItem.price : ProductItem.price * ProductItem.discount} </span>
+                    {ProductItem.discount < 1 && <span className={s.old_price}>{ProductItem.price}</span>}
+                </div>
+
                 <h3 className={s.product_title}>{ProductItem.title}</h3>
             </div>
         </li >);
